f4d1eb05e2979001d6a298696deb5475dd854318
max <jiayd163@163.com>
2023-09-14 19:20:07 +0800

    add file

---
+++ file/file.go
@@ -0,0 +1,51 @@
+package file
+
+import (
+	"fmt"
+	"os"
+	"sync"
+)
+
+// File is an io.WriteCloser that writes to the specified filename.
+// opens or creates the logfile on first Write.
+type File struct {
+	Filename string `json:"filename"`
+
+	file *os.File
+	mu   sync.Mutex
+}
+
+func NewFile(filename string) *File {
+	return &File{Filename: filename}
+}
+
+// Write to the file and create it if it does not exist
+func (f *File) Write(p []byte) (n int, err error) {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+
+	if f.file == nil {
+		f.file, err = os.OpenFile(f.Filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			return 0, fmt.Errorf("open file error: %s", err)
+		}
+		// 添加UTF-8编码声明
+		if _, err = f.file.Write([]byte{0xEF, 0xBB, 0xBF}); err != nil {
+			return 0, fmt.Errorf("write UTF-8 BOM error: %s", err)
+		}
+	}
+	return f.file.Write(p)
+}
+
+// Close implements io.Closer, and closes the current logfile.
+func (f *File) Close() error {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+
+	if f.file != nil {
+		err := f.file.Close()
+		f.file = nil
+		return err
+	}
+	return nil
+}
