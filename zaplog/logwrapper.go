643318646dec4654d4d77488938a79d16307c63a
max <max@163.com>
2024-04-30 18:58:18 +0800

    fix zaplog/logwrapper.go

--- zaplog/logwrapper.go
+++ zaplog/logwrapper.go
@@ -2,40 +2,40 @@ package zaplog
 
 // Debug logs a message at level Debug on the standard logger.
 func Debug(args ...interface{}) {
-	Logger.Sugar().Debug(args)
+	Logger.Sugar().Debug(args...)
 }
 
 // Info logs a message at level Info on the standard logger.
 func Info(args ...interface{}) {
-	Logger.Sugar().Info(args)
+	Logger.Sugar().Info(args...)
 }
 
 // Warn logs a message at level Warn on the standard logger.
 func Warn(args ...interface{}) {
-	Logger.Sugar().Warn(args)
+	Logger.Sugar().Warn(args...)
 }
 
 // Error logs a message at level Error on the standard logger.
 func Error(args ...interface{}) {
-	Logger.Sugar().Error(args)
+	Logger.Sugar().Error(args...)
 }
 
 // Debugf logs a message at level Debug on the standard logger.
 func Debugf(template string, args ...interface{}) {
-	Logger.Sugar().Debugf(template, args)
+	Logger.Sugar().Debugf(template, args...)
 }
 
 // Infof logs a message at level Info on the standard logger.
 func Infof(template string, args ...interface{}) {
-	Logger.Sugar().Info(template, args)
+	Logger.Sugar().Infof(template, args...)
 }
 
 // Warnf logs a message at level Warn on the standard logger.
 func Warnf(template string, args ...interface{}) {
-	Logger.Sugar().Warn(template, args)
+	Logger.Sugar().Warnf(template, args...)
 }
 
 // Errorf logs a message at level Error on the standard logger.
 func Errorf(template string, args ...interface{}) {
-	Logger.Sugar().Error(template, args)
+	Logger.Sugar().Errorf(template, args...)
 }
