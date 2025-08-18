7a2a1cf090684e712fa547772903dc9968525706
max <max@163.com>
2025-08-18 13:56:54 +0800

    fix ParseIPPrefix

--- util/ip.go
+++ util/ip.go
@@ -2,23 +2,18 @@ package util
 
 import (
 	"fmt"
-	"net"
+	"net/netip"
 )
 
-// ParseIPPrefix 解析 IP 字符串，如果是 IPv4 返回原地址，如果是 IPv6 返回 /64 前缀
+// ParseIPPrefix 解析 IP 字符串，如果是 IPv4 返回原地址，如果是 IPv6 返回前 64 位地址
 func ParseIPPrefix(ipStr string) (string, error) {
-	ip := net.ParseIP(ipStr)
-	if ip == nil {
-		return "", fmt.Errorf("invalid IP: %s", ipStr)
+	ip, err := netip.ParseAddr(ipStr)
+	if err != nil {
+		return "", fmt.Errorf("parse addr error: %s", err)
 	}
-
-	// IPv4 直接返回
-	if ip4 := ip.To4(); ip4 != nil {
-		return ip4.String(), nil
+	if ip.Is4() {
+		return ip.String(), nil
 	}
-
-	// IPv6 返回 /64 前缀
-	mask := net.CIDRMask(64, 128)
-	pref := ip.Mask(mask)
-	return pref.String() + "/64", nil
+	prefix := netip.PrefixFrom(ip, 64)
+	return prefix.Masked().Addr().String(), nil
 }
