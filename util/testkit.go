c1b4b98c586fccf91a573848eb0c770f4ecaf725
max <max@163.com>
2026-02-02 18:24:57 +0800

    fix testkit.go

--- util/testkit.go
+++ util/testkit.go
@@ -1,15 +1,15 @@
 package util
 
 import (
+	"context"
 	"fmt"
 	"net"
-
-	"github.com/valyala/fasthttp"
+	"net/http"
 )
 
 // StartTestServer starts a test server with a dynamic port and returns the host URL,
 // a cleanup function to shut down the server, and any error encountered.
-func StartTestServer(handler fasthttp.RequestHandler) (string, func(), error) {
+func StartTestServer(handler http.Handler) (string, func(), error) {
 	// Create a listener with a dynamic port
 	ln, err := net.Listen("tcp", ":0")
 	if err != nil {
@@ -21,7 +21,7 @@ func StartTestServer(handler fasthttp.RequestHandler) (string, func(), error) {
 	host := fmt.Sprintf("http://%s", addr)
 
 	// Start the server in a goroutine
-	srv := &fasthttp.Server{
+	srv := &http.Server{
 		Handler: handler,
 	}
 	go func() {
@@ -32,7 +32,7 @@ func StartTestServer(handler fasthttp.RequestHandler) (string, func(), error) {
 
 	// Return the host and a cleanup function to shut down the server
 	cleanup := func() {
-		srv.Shutdown()
+		srv.Shutdown(context.Background())
 		ln.Close()
 	}
 	return host, cleanup, nil
